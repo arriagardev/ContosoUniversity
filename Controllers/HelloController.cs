54321ba93169b5ecc26057f985dbd6fa2ec0ae75
Humberto Arriaga <arriagar@gmail.com>
2020-12-09 01:11:12 -0600

    hello controller and view

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int number = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = number;
+            return View();
+        }
+    }
+}
